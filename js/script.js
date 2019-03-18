e99c41743bcafeedceecd0c4e6cffef367162d8b
proweb4all <proweb4all@narod.ru>
2019-03-18 14:11:32 +0300

    yoga lesson11

--- js/script.js
+++ js/script.js
@@ -66,11 +66,11 @@ window.addEventListener('DOMContentLoaded', () => {
 
         let updateClock = () => {
             const t = getTimeRemaining(endtime);
-            console.log(t);
+            //console.log(t);
             hours.textContent = set2Char(t.hours);
             minutes.textContent = set2Char(t.minutes);
             seconds.textContent = set2Char(t.seconds);
-            console.log(hours.textContent, minutes.textContent, seconds.textContent);
+            //console.log(hours.textContent, minutes.textContent, seconds.textContent);
             (t.total <= 0) ? clearInterval(timeInterval) : {};
         };
         const timeInterval = setInterval(updateClock, 1000);
@@ -94,6 +94,49 @@ window.addEventListener('DOMContentLoaded', () => {
             document.body.style.overflow = '';
         });
     });
+
+    // Form
+    let message = {
+        loading: 'Загрузка...',
+        success: 'Спасибо! Скоро мы с вами свяжемся.',
+        failure: 'Что-то пошло не так...'
+    };
+    let form = document.querySelector('.main-form'),
+        input = form.getElementsByTagName('input'),
+        statusMessage = document.createElement('div');
+        statusMessage.classList.add('status');
+    form.addEventListener('submit', function(event){
+        event.preventDefault();
+        form.appendChild(statusMessage);
+        let request = new XMLHttpRequest();
+        request.open('POST', 'server.php');
+//        request.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+        request.setRequestHeader('Content-Type', 'application/json; charset=utf-8');
+        let formData = new FormData(form);
+        console.log('formData', formData);
+        let obj = {};
+        formData.forEach(function(value, key){
+            obj[key] = value;
+        });
+        console.log('obj', obj);
+        let json = JSON.stringify(obj);
+        request.send(json);
+        //request.send(formData);
+        request.addEventListener('readystatechange', function(){
+            if (request.readyState < 4){
+                statusMessage.innerHTML = message.loading;
+            } else if (request.readyState == 4 && request.status == 200){
+                statusMessage.innerHTML = message.success;
+            } else {
+                statusMessage.innerHTML = message.failure;
+            }
+        });
+        for (let i = 0; i < input.length; i++){
+            input[i].value = '';
+        }
+    })
+
+
 
 
 
